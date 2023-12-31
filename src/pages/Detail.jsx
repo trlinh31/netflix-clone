194746b7a93c7594343ffe9c53f45b5f824d5112
trlinh31 <nguyentranlinh31@gmail.com>
2023-12-31 13:46:48 +0700

    update

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -56,7 +56,7 @@ function Detail() {
             <div className="my-3">
               <h1 className="text-white font-medium text-2xl">{movie?.title}</h1>
             </div>
-            <div className="flex flex-wrap items-center my-3">
+            <div className="flex flex-wrap items-center gap-y-4 my-3">
               {genres.map((item) => (
                 <p
                   className="text-white mr-3 bg-primary px-3 py-1"
